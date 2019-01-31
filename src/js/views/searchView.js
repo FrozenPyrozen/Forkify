0a1279e7c934ba50f60fe64f2cbbf37657757d80
Frozen <kiril.kohan@gmail.com>
2019-01-31 21:35:59 +0200

    Added ... to UI title
    Created limitRecipeTitle() in searchView.js which allows us to putput limited tittle

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -3,11 +3,33 @@ import { elements } from "./base";
 export const getInput = () => elements.searchInput.value;
 
 export const clearInput = () => {
-  elements.searchInput.value = '';
+  elements.searchInput.value = "";
 };
 
 export const clearResults = () => {
-    elements.searchResList.innerHTML = '';
+  elements.searchResList.innerHTML = "";
+};
+// Pasta with tomato and spinach
+/*
+ *acc: 0 + cur.length = 0 + 5 // newTitle = ['Pasta']
+ *acc: 5 + cur.length = 5 + 4 // newTitle = ['Pasta', 'with']
+ *acc: 9 + cur.length = 9 + 6 // newTitle = ['Pasta', 'with', 'tomato']
+ *acc: 15 + cur.length = 15 + 3 // newTitle = ['Pasta', 'with', 'tomato']
+ *acc: 18 + cur.length = 18 + 7 // newTitle = ['Pasta', 'with', 'tomato']
+ */
+const limitRecipeTitle = (title, limit = 17) => {
+  const newTitle = [];
+  if (title.length > limit) {
+    title.split(' ').reduce((acc, cur) => {
+      if (acc + cur.length <= limit) {
+        newTitle.push(cur);
+      }
+      return acc + cur.length;
+    }, 0);
+    // return the result
+    return `${newTitle.join(' ')} ...`;
+  }
+  return title;
 };
 
 const renderRecipe = recipe => {
@@ -18,7 +40,9 @@ const renderRecipe = recipe => {
                     <img src="${recipe.image_url}" alt="${recipe.title}">
                 </figure>
                 <div class="results__data">
-                    <h4 class="results__name">${recipe.title}</h4>
+                    <h4 class="results__name">${limitRecipeTitle(
+                      recipe.title
+                    )}</h4>
                     <p class="results__author">${recipe.publisher}</p>
                 </div>
             </a>
