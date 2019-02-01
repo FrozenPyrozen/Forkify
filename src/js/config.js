d5a71831dc251f339f0eef5fe1de00299cd7a20f
Frozen <kiril.kohan@gmail.com>
2019-02-01 17:38:21 +0200

    Added Recipe model

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const proxy = 'https://cors-anywhere.herokuapp.com/';
+export const key = '029cf76af96f2ab1267615592368cd38';
+
+// API 815a83c58fc9d0ccc5510f0c9a5c27c1 // Working
+// URL https://www.food2fork.com/api/search
+// LOG afi06853@zwoho.com
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,15 +1,12 @@
 import axios from 'axios';
-// API 815a83c58fc9d0ccc5510f0c9a5c27c1 // Working
-// URL https://www.food2fork.com/api/search
-// LOG afi06853@zwoho.com
+import {key, proxy} from '../config'
+
 export default class Search {
   constructor(query) {
     this.query = query;
   }
 
   async getResults() {
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    const key = '029cf76af96f2ab1267615592368cd38';
     try {
       const res = await axios(
         `${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`
