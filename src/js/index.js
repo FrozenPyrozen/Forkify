2653bba92510429eba7e208dfd75fea6fd42da79
Frozen <kiril.kohan@gmail.com>
2019-02-01 16:40:04 +0200

    Added render animation and implemeted a search pagination

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Search from "./models/Search";
 import * as searchView from "./views/searchView";
-import { elements } from "./views/base";
+import { elements, renderLoader, clearLoader } from "./views/base";
 
 /** Global state of the app
  * - Seacrh object
@@ -21,11 +21,13 @@ const controlSearch = async () => {
     // 3) Prepare ui for results. Show a loading spinner
     searchView.clearInput();
     searchView.clearResults();
+    renderLoader(elements.searchRes);
 
     // 4) Search for recipes
     await state.search.getResults();
 
     // 5) Render results on UI
+    clearLoader();
     searchView.renderResults(state.search.result);
   }
 };
@@ -33,4 +35,13 @@ const controlSearch = async () => {
 elements.searchForm.addEventListener("submit", e => {
   e.preventDefault();
   controlSearch();
+});
+
+elements.searchResPages.addEventListener("click", e => {
+  const btn = e.target.closest(".btn-inline");
+  if (btn) {
+    const goToPage = parseInt(btn.dataset.goto, 10);
+    searchView.clearResults();
+    searchView.renderResults(state.search.result, goToPage);
+  }
 });
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -8,6 +8,7 @@ export const clearInput = () => {
 
 export const clearResults = () => {
   elements.searchResList.innerHTML = "";
+  elements.searchResPages.innerHTML = "";
 };
 // Pasta with tomato and spinach
 /*
@@ -20,14 +21,14 @@ export const clearResults = () => {
 const limitRecipeTitle = (title, limit = 17) => {
   const newTitle = [];
   if (title.length > limit) {
-    title.split(' ').reduce((acc, cur) => {
+    title.split(" ").reduce((acc, cur) => {
       if (acc + cur.length <= limit) {
         newTitle.push(cur);
       }
       return acc + cur.length;
     }, 0);
     // return the result
-    return `${newTitle.join(' ')} ...`;
+    return `${newTitle.join(" ")} ...`;
   }
   return title;
 };
@@ -51,6 +52,48 @@ const renderRecipe = recipe => {
   elements.searchResList.insertAdjacentHTML("beforeend", markup);
 };
 
-export const renderResults = recipes => {
-  recipes.forEach(renderRecipe);
+// type: 'prev' or 'next'
+const createButton = (page, type) => `
+    <button class="btn-inline results__btn--${type}" data-goto=${
+  type === "prev" ? page - 1 : page + 1
+}>
+        <span>Page ${type === "prev" ? page - 1 : page + 1}</span>
+        <svg class="search__icon">
+            <use href="img/icons.svg#icon-triangle-${
+              type === "prev" ? "left" : "right"
+            }"></use>
+        </svg>
+        
+    </button>
+`;
+
+const renderButtons = (page, numResults, resPerPage) => {
+  const pages = Math.ceil(numResults / resPerPage);
+
+  let button;
+  if (page === 1 && pages > 1) {
+    // Button go to next page
+    button = createButton(page, "next");
+  } else if (page < pages) {
+    // We want both buttons
+    button = `
+    ${createButton(page, "prev")}
+    ${createButton(page, "next")}
+    `;
+  } else if (page === pages && pages > 1) {
+    // Button go to previous page
+    button = createButton(page, "prev");
+  }
+  elements.searchResPages.insertAdjacentHTML("afterbegin", button);
+};
+
+export const renderResults = (recipes, page = 1, resPerPage = 10) => {
+  // Render results of current page
+  const start = (page - 1) * resPerPage;
+  const end = page * resPerPage;
+
+  recipes.slice(start, end).forEach(renderRecipe);
+
+  // Render pagination buttons
+  renderButtons(page, recipes.length, resPerPage);
 };
